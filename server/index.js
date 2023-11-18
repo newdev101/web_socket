1deed8df619692693d2cb677630bbfc7b0ba0567
rajat jana <rajatjana2024@gmail.com>
2023-11-18 18:03:35 +0530

    integrated express

--- server/index.js
+++ server/index.js
@@ -1,10 +1,28 @@
-import { createServer } from "http";
+// import { createServer } from "http";
+import express from 'express';
 import { Server } from "socket.io";
+import path  from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const PORT = process.env.PORT ||3500;
+
+// const httpServer = createServer();
+const app = express();
+
+
+//middleware
+app.use(express.static(path.join(__dirname, "public")));
 
 
 
-const httpServer = createServer();
-const io = new Server(httpServer,{
+const expressServer = app.listen(PORT, ()=>{
+     console.log(`listing to PORT=${PORT}`);
+});
+
+const io = new Server(expressServer,{
      //cros origin resource sharing
      cors:{
           origin: process.env.NODE_ENV === "production" ? flase : ["htttp://localhost:5500",'http://127.0.0.1:5500']
@@ -20,5 +38,3 @@ io.on('connection',socket=>{
      });
 });
 
-
-httpServer.listen(3500, ()=>console.log('listining i on 3500'));
